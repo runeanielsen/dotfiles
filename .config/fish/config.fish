30231ef087ec7d169bf3586c16f59f03f7f2f45f
Rune Nielsen <runeanielsen@live.dk>
2020-02-16 15:22:26 +0100

    Adds fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+set -x PATH $PATH /sbin/
+
+function ll
+    ls -lh $argv
+end
+
+set -U EDITOR nvim
+set -g theme_display_date no
+
+function fish_right_prompt; end
+function fish_greeting; end
+
+set -U fish_key_bindings fish_default_key_bindings
+
+if status --is-interactive
+    set PATH $PATH ~/.local/bin;
+end
