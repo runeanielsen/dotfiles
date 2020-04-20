7fbef1ac30a235d0265c48efee37f36cd5d5153d
Rune Nielsen <runeanielsen@live.dk>
2020-04-20 10:29:13 +0200

    decreases the emacs font size

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -80,8 +80,8 @@
 (add-hook! 'org-capture-mode-hook (company-mode -1))
 
 (setq
- doom-font (font-spec :family "monospace" :size 22 :weight 'bold)
- doom-variable-pitch-font (font-spec :family "monospace" :size 16)
+ doom-font (font-spec :family "monospace" :size 16 :weight 'bold)
+ doom-variable-pitch-font (font-spec :family "monospace" :size 14)
  dart-format-on-save t
  web-mode-markup-indent-offset 2
  web-mode-code-indent-offset 2
