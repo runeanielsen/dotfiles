0cf5fd15e235791e9951671def3f98e5369c6150
Rune Nielsen <runeanielsen@live.dk>
2020-06-13 02:11:20 +0200

    cleanup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -78,10 +78,6 @@
   (setq tab-width 4)
   (setq evil-shift-width 4)
 
-  ;csharp-mode README.md recommends this too
-  ;(electric-pair-mode 1)       ;; Emacs 24
-  ;(electric-pair-local-mode 1) ;; Emacs 25
-
   (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
   (local-set-key (kbd "C-c C-c") 'recompile)
   (local-set-key (kbd "C-c f f"), 'omnisharp-go-to-definition))
