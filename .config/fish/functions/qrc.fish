4db0e59b5b8f76a23317a8bea269f592a7ad88a3
Rune Nielsen <runeanielsen@live.dk>
2020-07-26 11:46:29 +0200

    adds alias for qrc

---
+++ .config/fish/functions/qrc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function qrc --wraps=/home/notation/.config/scripts/qrc.sh --description 'alias qrc /home/notation/.config/scripts/qrc.sh'
+  /home/notation/.config/scripts/qrc.sh  $argv;
+end
