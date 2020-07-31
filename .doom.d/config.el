0e794295a0833dfab2108149c5e2df1101d095f3
Rune Nielsen <runeanielsen@live.dk>
2020-07-31 08:47:35 +0200

    sets font size

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -31,8 +31,8 @@
 
 ;; Font ---------------------------------------------------------------
 (setq
- doom-font (font-spec :family "monospace" :size 20 :weight 'bold)
- doom-variable-pitch-font (font-spec :family "monospace" :size 20))
+ doom-font (font-spec :family "monospace" :size 15 :weight 'bold)
+ doom-variable-pitch-font (font-spec :family "monospace" :size 15))
 
 
 ;; Pandoc Mode --------------------------------------------------------
@@ -93,3 +93,49 @@
   (local-set-key (kbd "C-c f f"), 'omnisharp-go-to-definition))
 
 (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#171717" "#BF616A" "#A3BE8C" "#ECBE7B" "#8FA1B3" "#c678dd" "#46D9FF" "#c0c5ce"])
+ '(custom-safe-themes
+   (quote
+    ("d71aabbbd692b54b6263bfe016607f93553ea214bc1435d17de98894a5c3a086" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "82360e5f96244ce8cc6e765eeebe7788c2c5f3aeb96c1a765629c5c7937c0b5b" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" "3577ee091e1d318c49889574a31175970472f6f182a9789f1a3e9e4513641d86" default)))
+ '(fci-rule-color "#65737E")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#D08770"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#A3BE8C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#4f5b66"))
+ '(objed-cursor-color "#BF616A")
+ '(pdf-view-midnight-colors (cons "#c0c5ce" "#2b303b"))
+ '(rustic-ansi-faces
+   ["#2b303b" "#BF616A" "#A3BE8C" "#ECBE7B" "#8FA1B3" "#c678dd" "#46D9FF" "#c0c5ce"])
+ '(vc-annotate-background "#2b303b")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A3BE8C")
+    (cons 40 "#bbbe86")
+    (cons 60 "#d3be80")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e2ab77")
+    (cons 120 "#d99973")
+    (cons 140 "#D08770")
+    (cons 160 "#cc8294")
+    (cons 180 "#c97db8")
+    (cons 200 "#c678dd")
+    (cons 220 "#c370b6")
+    (cons 240 "#c16890")
+    (cons 260 "#BF616A")
+    (cons 280 "#a35f69")
+    (cons 300 "#875e68")
+    (cons 320 "#6b5c67")
+    (cons 340 "#65737E")
+    (cons 360 "#65737E")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
