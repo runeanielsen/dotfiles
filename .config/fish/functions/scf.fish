6488b94347310b2cbf339af8ba939ba7b00963cd
Rune Nielsen <runeanielsen@live.dk>
2020-09-27 23:04:28 +0200

    moved to scripts

--- .config/fish/functions/scf.fish
+++ .config/fish/functions/scf.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function scf --wraps=/home/notation/bin/snake-case-files.sh --description 'alias scf /home/notation/bin/snake-case-files.sh'
-  /home/notation/bin/snake-case-files.sh  $argv;
+function scf --wraps=/home/notation/.config/scripts/snake-case-files.sh --description 'alias scf /home/notation/.config/scripts/snake-case-files.sh'
+  /home/notation/.config/scripts/snake-case-files.sh  $argv;
 end
--- bin/snake-case-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -name "**" -exec bash -c 'mv "$0" "${0// /_}"' {} \;
-for f in `find`; do mv -v "$f" "`echo $f | tr '[A-Z]' '[a-z]'`"; done
