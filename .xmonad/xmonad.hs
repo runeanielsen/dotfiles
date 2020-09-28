bb963401fef5ef47c72dc71ee9c75e7008cdd697
Rune Nielsen <runeanielsen@live.dk>
2020-09-28 13:50:57 +0200

    decreases the icon spaceing

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -39,7 +39,7 @@ myFocusedBorderColor = "#ffffff"
 myStartupHook :: X ()
 myStartupHook = do
   spawnOnce "nitrogen --restore &" 
-  spawn "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true --iconspacing 6 --tint 0x00000 --transparent true --alpha 0 &"
+  spawn "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true --iconspacing 4 --tint 0x00000 --transparent true --alpha 0 &"
   setWMName "LG3D"
 
 -- Layout
