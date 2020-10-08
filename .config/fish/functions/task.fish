9663d567552eb2d952f81e2c18209c01862b735a
Rune Nielsen <runeanielsen@live.dk>
2020-10-08 20:48:19 +0200

    updates fish configs

--- .config/fish/functions/task.fish
+++ .config/fish/functions/task.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function task --wraps=./bin/task --wraps=/home/notation/bin/task --description 'alias task /home/notation/bin/task'
-  /home/notation/bin/task  $argv;
+function task --wraps=./bin/task --wraps=/home/notation/bin/task --wraps=/home/notation/.config/scripts/task --description 'alias task /home/notation/.config/scripts/task'
+  /home/notation/.config/scripts/task  $argv;
 end
