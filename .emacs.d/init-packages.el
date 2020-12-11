754c76e0be9c14c0c98c90d2d27dbb7936829f1d
Rune Nielsen <runenielsen@runbox.com>
2020-12-11 10:19:49 +0100

    moves packages to init-packages file

---
+++ .emacs.d/init-packages.el
@@ -0,0 +1,44 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+; list the packages you want
+(setq package-list
+      '(
+        use-package
+        evil
+        lsp-mode
+        lsp-ui
+        company
+        lsp-ivy
+        which-key
+        flycheck
+        go-mode
+        lsp-python-ms
+        csharp-mode
+        web-mode
+        js2-mode
+        json-mode
+        doom-modeline
+        doom-themes
+        theme-magic
+        treemacs
+        projectile
+        treemacs-projectile
+        evil-leader
+        evil-snipe
+        tide
+        eslint-fix))
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
