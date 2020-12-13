d14506e18c0398c36b419590697715fc2678c7ab
Rune Nielsen <runenielsen@runbox.com>
2020-12-13 21:34:51 +0100

    sets default theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -111,7 +111,7 @@ enable-recursive-minibuffers t)
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;;(load-theme 'doom-spacegrey t)
+  (load-theme 'doom-spacegrey t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
