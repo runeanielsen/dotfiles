265205f42aa5d2b0eac1a68e9fdfe8dfb9329d3b
Rune Nielsen <runenielsen@runbox.com>
2020-12-17 15:55:24 +0100

    fixes python lsp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -266,7 +266,8 @@
 ;; --- python-ms ---
 (use-package lsp-python-ms
   :ensure t
-  :init (setq lsp-python-ms-auto-install-server t)
+  :init
+  (setq lsp-python-ms-auto-install-server t)
   :hook (python-mode . (lambda ()
                           (require 'lsp-python-ms)
                           (lsp))))
