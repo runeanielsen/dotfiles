20959be8441d1e68a162404e9816340e8ebcc739
Rune Nielsen <runenielsen@runbox.com>
2020-12-17 19:34:00 +0100

    adds markdown mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -490,4 +490,21 @@
     (add-hook 'js2-mode-hook 'prettier-js-mode)
     (add-hook 'web-mode-hook 'prettier-js-mode)
     (add-hook 'scss-mode-hook 'prettier-js-mode))
+
+;; --- markdown ---
+(use-package markdown-mode
+  :ensure t
+  :mode "\\.md\\'"
+  :config
+  (setq markdown-command "marked")
+  (defun dw/set-markdown-header-font-sizes ()
+    (dolist (face '((markdown-header-face-1 . 1.2)
+                    (markdown-header-face-2 . 1.1)
+                    (markdown-header-face-3 . 1.0)
+                    (markdown-header-face-4 . 1.0)
+                    (markdown-header-face-5 . 1.0)))
+      (set-face-attribute (car face) nil :weight 'normal :height (cdr face))))
+  (defun dw/markdown-mode-hook ()
+    (dw/set-markdown-header-font-sizes))
+  (add-hook 'markdown-mode-hook 'dw/markdown-mode-hook))
 ;;; init.el ends here
