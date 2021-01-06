a5b02ee3c77a1b83b78c1c2416dde63bf49bd6e9
Rune Nielsen <runenielsen@runbox.com>
2021-01-06 06:11:52 +0100

    cleanup md mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -555,20 +555,22 @@
          (css-mode . prettier-js-mode)))
 
 ;; --- markdown ---
+(defun fp/set-markdown-header-font-sizes ()
+  (dolist (face '((markdown-header-face-1 . 1.2)
+                  (markdown-header-face-2 . 1.1)
+                  (markdown-header-face-3 . 1.0)
+                  (markdown-header-face-4 . 1.0)
+                  (markdown-header-face-5 . 1.0)))
+    (set-face-attribute (car face) nil :weight 'normal :height (cdr face))))
+
+(defun fp/markdown-mode-hook ()
+  (fp/set-markdown-header-font-sizes))
+
 (use-package markdown-mode
   :mode "\\.md\\'"
   :config
   (setq markdown-command "marked")
-  (defun dw/set-markdown-header-font-sizes ()
-    (dolist (face '((markdown-header-face-1 . 1.2)
-                    (markdown-header-face-2 . 1.1)
-                    (markdown-header-face-3 . 1.0)
-                    (markdown-header-face-4 . 1.0)
-                    (markdown-header-face-5 . 1.0)))
-      (set-face-attribute (car face) nil :weight 'normal :height (cdr face))))
-  (defun dw/markdown-mode-hook ()
-    (dw/set-markdown-header-font-sizes))
-  (add-hook 'markdown-mode-hook 'dw/markdown-mode-hook))
+  (add-hook 'markdown-mode-hook 'fp/markdown-mode-hook))
 
 ;; --- Org mode ---
 (defun fp/org-mode-setup ()
