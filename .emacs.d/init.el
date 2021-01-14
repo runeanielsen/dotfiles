00c23e69ed15161f835e50bb36bc680b12229004
Rune Nielsen <runenielsen@runbox.com>
2021-01-15 00:29:50 +0100

    switches theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -199,7 +199,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-horizon t)
+  (load-theme 'doom-nord t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
