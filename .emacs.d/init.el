b9d06485bbc7e952a86ecd7bff75f98157082281
Rune Nielsen <runenielsen@runbox.com>
2021-02-15 21:46:01 +0100

    removes org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -663,28 +663,5 @@
   (setq markdown-command "marked")
   (add-hook 'markdown-mode-hook 'fp/markdown-mode-hook))
 
-;; --- Org mode ---
-(defun fp/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-(use-package org
-  :hook (org-mode . fp/org-mode-setup))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
-
 ;; Make gc pause faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
