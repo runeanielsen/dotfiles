c64a352674d761822dfdd687e1540d14aa2517a8
Rune Nielsen <runenielsen@runbox.com>
2021-02-15 21:57:11 +0100

    removes auto-update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,15 +19,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package auto-package-update
-  :custom
-  (auto-package-update-interval 7)
-  (auto-package-update-prompt-before-update t)
-  (auto-package-update-hide-results t)
-  :config
-  (auto-package-update-maybe)
-  (auto-package-update-at-time "09:00"))
-
 ;; Font
 (set-face-attribute 'default nil :font "Fira Code" :height 115)
 
