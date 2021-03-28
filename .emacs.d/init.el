de3ad37bbbdd51baeccbfaeb1ce474ae0d406da2
Rune Nielsen <runenielsen@runbox.com>
2021-03-28 20:49:16 +0200

    removes evil motion tracker

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -132,14 +132,6 @@
   (when noninteractive
     (shut-up-silence-emacs)))
 
-(use-package evil-motion-trainer
- :straight '(evil-motion-trainer :host github
-                         :repo "martinbaillie/evil-motion-trainer"
-                         :branch "master")
- :config
- (global-evil-motion-trainer-mode 1))
-
-
 ;; --- yasnippet ---
 (use-package yasnippet
   :config
