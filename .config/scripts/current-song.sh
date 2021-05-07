cb565a640010733b3e2bc0b55e0c9490c2446ecf
Rune Nielsen <runenielsen@runbox.com>
2021-05-07 23:44:04 +0200

    display song time better

--- .config/scripts/current-song.sh
+++ .config/scripts/current-song.sh
@@ -5,11 +5,31 @@ song=$(cmus-remote -Q | sed -n -e 's/^.*tag title //p')
 position=$(cmus-remote -Q | sed -n -e 's/^.*position //p')
 duration=$(cmus-remote -Q | sed -n -e 's/^.*duration //p')
 
+make_into_time() {
+    local min=`expr $1 / 60`
+    local sec=`expr $1 % 60`
+
+    if [ `expr length $min` -eq 1 ]
+    then
+        local min="0"$min
+    fi
+
+    if [ `expr length $sec` -eq 1 ]
+    then
+        local sec="0"$sec
+    fi
+
+    make_into_time_result=$min:$sec
+}
+
 if [ -z "$song" ]
 then
-        echo ""
+    echo ""
 else
-        position_time=`expr $position / 60`:`expr $position % 60`
-        duration_time=`expr $duration / 60`:`expr $duration % 60`
-        echo "$artist: $song ($position_time/$duration_time)"
+    make_into_time $position
+    position_time=$make_into_time_result
+    make_into_time $duration
+    duration_time=$make_into_time_result
+
+    echo "$artist: $song ($position_time/$duration_time)"
 fi
