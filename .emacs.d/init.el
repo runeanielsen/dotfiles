46a1ab08556794c0649970a2e3544349d12d7fbb
Rune Nielsen <runenielsen@runbox.com>
2021-09-02 20:31:59 +0200

    fixes font-size issue for org levels

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -783,20 +783,31 @@
 
 ;; --- org-mode ---
 (defun fp/org-font-setup ()
-  "Org-mode font setup."
+  "Org mode font setup."
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
                           '(("^ *\\([-]\\) "
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.15)
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.3)
+                  (org-level-2 . 1.2)
                   (org-level-3 . 1.1)
-                  (org-level-4 . 1.0)
+                  (org-level-4 . 1.05)
                   (org-level-5 . 1.0)
                   (org-level-6 . 1.0)
                   (org-level-7 . 1.0)
-                  (org-level-8 . 1.0)))))
+                  (org-level-8 . 1.0)))
+    (set-face-attribute (car face) nil :font "Fira Code" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
 (use-package org
   :hook ((org-mode . org-indent-mode))
