88288075619eac74c299d5ff1e232c9a01c72db9
Rune Nielsen <runenielsen@runbox.com>
2021-09-13 20:36:53 +0200

    adds org roam

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -835,4 +835,16 @@
   :straight nil
   :after org)
 
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/roam-notes")
+  (org-roam-complete-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
 ;;; init.el ends here
