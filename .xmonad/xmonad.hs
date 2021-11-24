2c0eb7b3f78b2c31cffd634a8ac34ee15ab8867e
Rune Nielsen <runenielsen@runbox.com>
2021-11-24 23:11:18 +0100

    adds xcompmgr to startup hook

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -39,6 +39,7 @@ myFocusedBorderColor = "#ffffff"
 myStartupHook :: X ()
 myStartupHook = do
   spawn "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true --iconspacing 4 --tint 0x000000 --transparent true --alpha 0 &"
+  spawn "xcompmgr -a &"
   spawn "nitrogen --restore &"
   spawn "xset r rate 250 25 &"
   spawn "xset s off -dpms &"
