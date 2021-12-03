9c641ded4d73f21e47264649d387e89e888234f9
Rune Nielsen <runenielsen@runbox.com>
2021-12-03 17:13:00 +0100

    decrease r rate

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -41,7 +41,7 @@ myStartupHook = do
   spawn "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true --iconspacing 4 --tint 0x000000 --transparent true --alpha 0 &"
   spawn "xcompmgr -a &"
   spawn "nitrogen --restore &"
-  spawn "xset r rate 250 25 &"
+  spawn "xset r rate 225 25 &"
   spawn "xset s off -dpms &"
   spawn "unclutter -idle 5 &"
   setWMName "LG3D"
