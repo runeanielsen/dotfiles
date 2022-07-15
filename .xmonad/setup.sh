dbe61c959813291e281228208aab5514b71367af
Rune Nielsen <runenielsen@runbox.com>
2022-07-15 19:07:25 +0200

    adds setup script for xmonad

---
+++ .xmonad/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+git clone "https://github.com/xmonad/xmonad" xmonad-git
+git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
+git clone "https://github.com/jaor/xmobar" xmobar-git
