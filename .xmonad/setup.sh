31199782f980c8eaed3d510210fa246dedf4c704
Rune Nielsen <runenielsen@runbox.com>
2022-07-16 17:02:12 +0200

    now points to codeberg for xmobar

--- .xmonad/setup.sh
+++ .xmonad/setup.sh
@@ -2,4 +2,4 @@
 
 git clone "https://github.com/xmonad/xmonad" xmonad-git
 git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
-git clone "https://github.com/jaor/xmobar" xmobar-git
+git clone "https://codeberg.org/xmobar/xmobar.git" xmobar-git
