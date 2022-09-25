ed232546cd019c13c224b23bcd297c646713a71b
Rune Nielsen <runenielsen@runbox.com>
2022-09-25 22:09:05 +0200

    improves function naming

--- .config/scripts/current-song.clj
+++ .config/scripts/current-song.clj
@@ -3,9 +3,8 @@
 (require '[clojure.java.shell :as shell]
          '[clojure.string :as str])
 
-(defn information-song-field [song-information field]
-  (when-let [tag (first (filter #(str/starts-with? % field) song-information))]
-    (str/replace tag field "")))
+(defn query-current-song! []
+  (shell/sh "cmus-remote" "-Q"))
 
 (defrecord CurrentSong
     [status
@@ -14,38 +13,39 @@
      position
      duration])
 
+(defn get-song-field [song-information field]
+  (when-let [tag (first (filter #(str/starts-with? % field) song-information))]
+    (str/replace tag field "")))
+
 (defn make-current-song [song-information]
   (map->CurrentSong
-   {:status (or (information-song-field song-information "status ") "")
-    :artist (or (information-song-field song-information "tag artist ") "")
-    :title (or (information-song-field song-information "tag title ") "")
-    :position (Integer. (or (information-song-field song-information "position ") 0))
-    :duration (Integer. (or (information-song-field song-information "duration ") 0))}))
+   {:status (or (get-song-field song-information "status ") "")
+    :artist (or (get-song-field song-information "tag artist ") "")
+    :title (or (get-song-field song-information "tag title ") "")
+    :position (Integer. (or (get-song-field song-information "position ") 0))
+    :duration (Integer. (or (get-song-field song-information "duration ") 0))}))
 
-(defn get-song-information! []
-  (shell/sh "cmus-remote" "-Q"))
-
-(defn convert-to-time [t]
+(defn format-time [t]
   (let [min (int (/ t 60))
         sec (mod t 60)]
     (format "%02d:%02d" min sec)))
 
-(defn format-song-information [current-song]
+(defn format-song [current-song]
   (if (= (:status current-song) "playing")
     (let [artist (:artist current-song)
           title (:title current-song)
-          position (convert-to-time (:position current-song))
-          duration (convert-to-time (:duration current-song))]
+          position (format-time (:position current-song))
+          duration (format-time (:duration current-song))]
       (format "%s : %s (%s/%s)" artist title position duration))
     ""))
 
 (defn main []
-  (let [song-information (get-song-information!)]
+  (let [song-information (query-current-song!)]
     (if (= (:exit song-information) 0)
       (->> (:out song-information)
            (str/split-lines)
            (make-current-song)
-           (format-song-information)
+           (format-song)
            (println))
       "")))
 
