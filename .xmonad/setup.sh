61954acca2d2968c8b42272106337c8f9474156d
Rune Nielsen <runenielsen@runbox.com>
2023-01-14 16:49:10 +0000

    set specific version for build

--- .xmonad/setup.sh
+++ .xmonad/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
-git clone "https://github.com/xmonad/xmonad" xmonad-git
-git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
-git clone "https://codeberg.org/xmobar/xmobar.git" xmobar-git
+set -e
+
+git clone -b v0.17.1 "https://github.com/xmonad/xmonad" xmonad-git
+git clone -b v0.17.1 "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
+git clone -b 0.44.1 "https://codeberg.org/xmobar/xmobar.git" xmobar-git
