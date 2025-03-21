ecccb515b6b32c62bc9e64b3d073206e858ebc18
runeanielsen <runenielsen@runbox.com>
2025-03-21 08:28:06 +0100

    remove xmonad setup after switch to wayland

--- .xmonad/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -e
-
-git clone -b v0.17.1 "https://github.com/xmonad/xmonad" xmonad-git
-git clone -b v0.17.1 "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
-git clone -b 0.44.1 "https://codeberg.org/xmobar/xmobar.git" xmobar-git
